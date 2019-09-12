3ce4894046cad2275302f1f564da7589dc4c7fb1
Igorcapatina <capatina7802@gmail.com>
2019-09-12 12:49:59 -0900

    Change Code Num2

---
+++ Week02/Igor/02_PoundToKg/app.js
@@ -0,0 +1,11 @@
+function convertA(){
+    let input = document.getElementById("num1");
+    let n1 = input.value;
+    document.getElementById("num2").innerText = n1 * 2.2;
+}
+
+function convertB(){
+    let input = document.getElementById("num3");
+    let n2 = input.value;
+    document.getElementById("num4").innerText = n2 / 2.2;
+}
