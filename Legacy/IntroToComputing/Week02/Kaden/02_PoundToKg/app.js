fa77fb95db7afcf6bdf299db070eb66c52257c92
ciccc-wmad-0903 <ciccc.wmad.0903@gmail.com>
2019-09-30 11:38:06 -0700

    Delete Legacy files

--- Legacy/IntroToComputing/Week02/Kaden/02_PoundToKg/app.js
+++
@@ -1,7 +0,0 @@
-function weightConverter(num) {
-    if (document.activeElement.id === "inputLB") {
-        document.getElementById("outputKG").innerText = num / 2.2;
-    } else {
-        document.getElementById("outputLB").innerText = num * 2.2;
-    }
-}
