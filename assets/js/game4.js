cf52dbdf1041033eb8d2e0335f913ff77b7fb3cd
Chris <chrisweng71@outlook.com>
2019-10-03 11:35:43 -0700

    Game4 fixed

--- assets/js/game4.js
+++ assets/js/game4.js
@@ -3,14 +3,14 @@ const PRIZE_LENGTH = 3;
 function result(){
     var prizeArray = [];
     while (prizeArray.length < PRIZE_LENGTH) {
-        var temp = Math.floor(Math.random() * 100)+1;
+        var temp = Math.floor(Math.random() * 10)+1;
         if (!prizeArray.includes(temp)) {
             prizeArray.push(temp);
         }
     }
-    var d = parseInt(document.getElementById("fin").value);
-    var e = parseInt(document.getElementById("sin").value);
-    var f = parseInt(document.getElementById("tin").value);
+    var d = parseInt(document.getElementById("g4_fin").value);
+    var e = parseInt(document.getElementById("g4_sin").value);
+    var f = parseInt(document.getElementById("g4_tin").value);
     if(d == e || d == f || e == f){
         alert("please put different number");
         return;
@@ -24,15 +24,15 @@ function result(){
             numNoAppear++;
         }
     }
-    document.getElementById("output").innerHTML= prizeArray;
+    document.getElementById("g4_output").innerHTML= prizeArray;
     if(indexArray[0] == 0 && indexArray[1] == 1 && indexArray[2] == 2) {
-        document.getElementById("output1").innerHTML="Congrats , your number is " + inputArray + " number and index are same as our number. You win $10000!!";
+        document.getElementById("g4_output1").innerHTML="Congrats , your number is " + inputArray + " number and index are same as our number. You win $10000!!";
     }else if(indexArray[0] != -1 && indexArray[1] != -1 && indexArray[2] != -1) {
-        document.getElementById("output1").innerHTML="Congrats , your number is " + inputArray + " number are same as our number, but index is different. You win $3000!!";
+        document.getElementById("g4_output1").innerHTML="Congrats , your number is " + inputArray + " number are same as our number, but index is different. You win $3000!!";
     }else if(numNoAppear == 1) {
-        document.getElementById("output1").innerHTML="Congrats , your number is " + inputArray + " two number are same as our number. You win $1000!!";
+        document.getElementById("g4_output1").innerHTML="Congrats , your number is " + inputArray + " two number are same as our number. You win $1000!!";
     }else{
-        document.getElementById("output1").innerHTML="Sorry , your number is " + inputArray + " you can't win the prize...";
+        document.getElementById("g4_output1").innerHTML="Sorry , your number is " + inputArray + " you can't win the prize...";
     }
 }
 
