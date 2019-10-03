9e9c89e77cee366fc72ced4a4022ff9864d35cad
Chris <chrisweng71@outlook.com>
2019-10-03 09:47:43 -0700

    Game4 draft

---
+++ assets/js/game4.js
@@ -0,0 +1,38 @@
+const PRIZE_LENGTH = 3;
+
+function result(){
+    var prizeArray = [];
+    while (prizeArray.length < PRIZE_LENGTH) {
+        var temp = Math.floor(Math.random() * 100)+1;
+        if (!prizeArray.includes(temp)) {
+            prizeArray.push(temp);
+        }
+    }
+    var d = parseInt(document.getElementById("fin").value);
+    var e = parseInt(document.getElementById("sin").value);
+    var f = parseInt(document.getElementById("tin").value);
+    if(d == e || d == f || e == f){
+        alert("please put different number");
+        return;
+    }
+    var inputArray = [d,e,f];
+    var numNoAppear = 0;
+    var indexArray = [];
+    for (var i = 0; i < inputArray.length; ++i) {
+        indexArray[i] = prizeArray.indexOf(inputArray[i]);
+        if (indexArray[i] < 0) {
+            numNoAppear++;
+        }
+    }
+    document.getElementById("output").innerHTML= prizeArray;
+    if(indexArray[0] == 0 && indexArray[1] == 1 && indexArray[2] == 2) {
+        document.getElementById("output1").innerHTML="Congrats , your number is " + inputArray + " number and index are same as our number. You win $10000!!";
+    }else if(indexArray[0] != -1 && indexArray[1] != -1 && indexArray[2] != -1) {
+        document.getElementById("output1").innerHTML="Congrats , your number is " + inputArray + " number are same as our number, but index is different. You win $3000!!";
+    }else if(numNoAppear == 1) {
+        document.getElementById("output1").innerHTML="Congrats , your number is " + inputArray + " two number are same as our number. You win $1000!!";
+    }else{
+        document.getElementById("output1").innerHTML="Sorry , your number is " + inputArray + " you can't win the prize...";
+    }
+}
+
